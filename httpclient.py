1b103211a63297057b3c565821390cbbec43af57
Mark Galloway <mgallowa@ualberta.ca>
2015-01-26 11:45:01 -0700

    GET working for all but internet_gets

--- httpclient.py
+++ httpclient.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 # Copyright 2013 Abram Hindle
-# 
+# Copyright 2015 Mark Galloway
+# Copyright 2015 Tamara Bain
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,29 +26,87 @@ import re
 # you may use urllib to encode data appropriately
 import urllib
 
+
 def help():
     print "httpclient.py [GET/POST] [URL]\n"
+
 
 class HTTPRequest(object):
     def __init__(self, code=200, body=""):
         self.code = code
         self.body = body
 
-class HTTPClient(object):
-    #def get_host_port(self,url):
 
+class HTTPClient(object):
+    # def get_host_port(self,url):
+
+    # Look up host by name
+    def get_hostname(self, host):
+        try:
+            return socket.gethostbyname(host)
+        except socket.gaierror:
+            print 'Hostname could not be resolved. Exiting'
+            sys.exit()
+
+    # Connect to host/port via sockets
     def connect(self, host, port):
-        # use sockets!
-        return None
+        self.sock = None
+        try:
+            # Create an AF_INET, STREAM socket (TCP)
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except socket.error, msg:
+            print 'Failed to create socket. Error code: ' \
+                  + str(msg[0]) + ' , Error message : ' + msg[1]
+            sys.exit()
+
+        remote_ip = self.get_hostname(host)
+        # Establish connection
+        self.sock.connect((remote_ip, port))
+
+    # Disconnect socket
+    def disconnect(self):
+        self.sock.close()
 
     def get_code(self, data):
-        return None
+        return int(data.split()[1])
 
-    def get_headers(self,data):
-        return None
+    def get_headers(self, data):
+        start = data.find("\r\n")
+        end = data.find("\r\n\r\n")
+        if start == -1:
+            return ''
+        else :
+            return data[start + 2:end]
 
     def get_body(self, data):
-        return None
+        start = data.find("\r\n\r\n")
+        if start == -1:
+            return ''
+        else :
+            return data[start + 4:]
+
+    def send_and_receive(self, host, port, request):
+        # Connect To Host
+        self.connect(host, port)
+
+        # Send Request
+        try:
+            self.sock.sendall(request.encode("UTF8"))
+        except socket.error:
+            # Send failed
+            print 'Send failed'
+            sys.exit()
+
+        # Receive Data
+        data = self.recvall(self.sock)
+
+        # DEBUG
+        # print(data)
+
+        # Disconnect From Host
+        self.disconnect()
+
+        return data
 
     # read everything from the socket
     def recvall(self, sock):
@@ -60,11 +120,46 @@ class HTTPClient(object):
                 done = not part
         return str(buffer)
 
+    # Break down a URL into its host,port,path components
+    def decomposeUrl(self, url):
+        regex = "(?:http://)?(?P<host>[^:/]+).?(?P<port>[0-9]*)(?P<path>.*)"
+        match = re.search(regex, url)
+        host = match.group('host')
+        port = match.group('port')
+        if port == '':
+            port = 80
+        else:
+            port = int(port)
+        path = match.group('path')
+        if path == '' or path[0] != '/':
+            path = '/' + path
+        return host, port, path
+
+    # Perform a HTTP GET request to the URL argument
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        # Build GET Request
+        host, port, path = self.decomposeUrl(url)
+        encoded_path = urllib.quote(path)
+        encoded_host = urllib.quote(host)
+
+        request = "GET " + encoded_path + " HTTP/1.1\r\n" \
+            + "User-Agent: cmput410client\r\n" \
+            + "Host: " + encoded_host + "\r\n" \
+            + "Accept: */*\r\n\r\n"
+
+        # Send the request and get the response
+        data = self.send_and_receive(host, port, request)
+
+        # DEBUG
+        # print(data)
+
+        # Parse Data
+        code = self.get_code(data)
+        body = self.get_body(data)
+
         return HTTPRequest(code, body)
 
+    # TODO: encode query arguments using urllib
     def POST(self, url, args=None):
         code = 500
         body = ""
@@ -72,10 +167,10 @@ class HTTPClient(object):
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
-            return self.POST( url, args )
+            return self.POST(url, args)
         else:
-            return self.GET( url, args )
-    
+            return self.GET(url, args)
+
 if __name__ == "__main__":
     client = HTTPClient()
     command = "GET"
@@ -83,6 +178,6 @@ if __name__ == "__main__":
         help()
         sys.exit(1)
     elif (len(sys.argv) == 3):
-        print client.command( sys.argv[1], sys.argv[2] )
+        print client.command(sys.argv[2], sys.argv[1])
     else:
-        print client.command( command, sys.argv[1] )    
+        print client.command(sys.argv[1], command)
---
+++ ourtests.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Copyright 2015 Mark Galloway
+# Copyright 2015 Tamara Bain
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# run python ourtests.py
+
+import unittest
+import httpclient
+
+
+class TestHTTPClient(unittest.TestCase):
+
+    def testConnect(self):
+        http = httpclient.HTTPClient()
+        http.connect("www.google.ca", 80)
+        http.disconnect()
+
+    def testget_hostname(self):
+        http = httpclient.HTTPClient()
+        http.get_hostname("www.google.ca")
+
+    def test_full_url(self):
+        http = httpclient.HTTPClient()
+        host, port, path = http.decomposeUrl("http://www.google.ca:80/some/path/index.html")
+        self.assertTrue(host == "www.google.ca")
+        self.assertTrue(port == 80)
+        self.assertTrue(path == "/some/path/index.html")
+
+    def test_no_port(self):
+        http = httpclient.HTTPClient()
+        host, port, path = http.decomposeUrl("http://www.google.ca/some/path/index.html")
+        self.assertTrue(host == "www.google.ca")
+        self.assertTrue(port == 80)
+        self.assertTrue(path == "/some/path/index.html")
+
+    def test_no_path(self):
+        http = httpclient.HTTPClient()
+        host, port, path = http.decomposeUrl("http://www.google.ca:75")
+        self.assertTrue(host == "www.google.ca")
+        self.assertTrue(port == 75)
+        self.assertTrue(path == "/")
+
+    def test_no_path_no_port(self):
+        http = httpclient.HTTPClient()
+        host, port, path = http.decomposeUrl("www.google.ca")
+        self.assertTrue(host == "www.google.ca")
+        self.assertTrue(port == 80)
+        self.assertTrue(path == "/")
+
+    def test_get_code(self):
+        http = httpclient.HTTPClient()
+        response = "HTTP/1.1 302 Found"
+        code = http.get_code(response)
+        self.assertTrue(code == 302)
+
+if __name__ == '__main__':
+    unittest.main()
